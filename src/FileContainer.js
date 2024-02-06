6630e3cf84c1a87b977c93c41e7b92840862ddc3
Mackenzie <84205474+MackenzieI@users.noreply.github.com>
2024-02-06 11:30:35 -0600

    Update FileContainer.js, started implementation of file upload
    Included: 
    -fileTitle, fileContent, and file useStates
    -file submit form 
    -handle functions
    
    Dependencies:
    -uses axios for http gets
    
    Credits:
    https://www.filestack.com/fileschool/react/react-file-upload/
    
    Bugs: 
    -when clicking upload citation, TypeError: Cannot read properties of undefined (reading 'name')" error is thrown.

--- src/FileContainer.js
+++ src/FileContainer.js
@@ -1,16 +1,34 @@
+import { useState } from "react";
+import axios from 'axios';
+
 function FileContainer() {
-    let fileTitle = "File";
-    let fileContent = `@book{key ,
-        author = {L M Napster},
-        title = {Mathematical Theory of Efficient Piracy},
-        series = {Lecture Notes in Mathematics},
-        volume = {3204},
-        publisher = {Springer Verlag},
-        address = {New York NY},
-        year = {1998}
-        }
-        `;
+    let [fileTitle, setFileTitle] = useState("Untitled");
+    let [fileContent, setFileContent] = useState("There are no citations added. Please upload or create a citation.");
         //Using <pre> to keep formatting. This is not a preferred way.
+        //Set fileContent to whatever the user gives it.
+    const [file, setFile] = useState()
+
+    function handleChange(event) {
+          setFile(event.target.files[0])
+        }
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        const url = 'http://localhost:3000/uploadFile';
+        const formData = new FormData();
+        formData.append('file', file);
+        formData.append('fileName', file.name);
+        const config = {
+            headers: {
+                'content-type': 'multipart/form-data',
+            },
+        };
+        axios.post(url, formData, config).then((response) => {
+            setFileContent(response.data.fileUrl);
+        });
+    }
+    /*upload citation throws a "Cannot read properties of undefined (reading 'name')
+TypeError: Cannot read properties of undefined (reading 'name')" error. Used https://www.filestack.com/fileschool/react/react-file-upload/*/
     return (
     <div id="file" className="container-fluid"> 
         <div className="row">
@@ -20,6 +38,10 @@ function FileContainer() {
                     <p>{fileContent}</p> 
                     
                 </pre></div>
+                <form onSubmit={handleSubmit}>
+                    <input type="file" />
+                    <button type="submit" onChange={handleChange}>Upload citation</button> 
+                </form>
             </div>
         </div>
     </div>
