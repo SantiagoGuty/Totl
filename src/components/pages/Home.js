f7fb2881084d9ff5812a7df3e5c4f41f5e2d13ce
Martha Aponbiede <105121021+maponbie@users.noreply.github.com>
2024-02-12 16:17:39 -0600

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,16 +1,20 @@
-import React from "react";
-import "./Home.css"; 
+import React, { useState } from "react";
+import { Citations } from "./links/Citations"; 
+import "./Home.css";
 
 function Home() {
-  return (
-    <div className="home-container">
-      <div className="home-search-bar">
-        <input type="text" placeholder="Search..." className="home-search-input" />
-        <button className="home-cite-button">Cite</button>
-      </div>
-      <button className="home-upload-button">Upload Manually</button>
-    </div>
-  );
+    const [showCitations, setShowCitations] = useState(false);
+
+    return (
+        <div className="home-container">
+            <div className="home-search-bar">
+                <input type="text" placeholder="Search..." className="home-search-input" />
+                <button className="home-cite-button">Cite</button>
+            </div>
+            <button className="home-upload-button" onClick={() => setShowCitations(true)}>Upload Manually</button>
+            {showCitations && <Citations />}
+        </div>
+    );
 }
 
 export default Home;
